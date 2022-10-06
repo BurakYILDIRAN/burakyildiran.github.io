d8005c2774bb49b1afde5814c8048dadef333076
BurakYILDIRAN <brkyldrn@gmail.com>
2022-10-06 13:31:46 +0300

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,28 +1,5 @@
 <?php
-// define variables and set to empty values
-$nameErr = $emailErr = $messageErr = "";
-$name = $email = $message = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-  }
-
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-  }
-
- 
-  if (empty($_POST["message"])) {
-    $message = "";
-  } else {
-    $message = test_input($_POST["message"]);
-  }
-
-  
-}
+   echo("Name: " . $_POST['name'] . "<br />\\n");
+   echo("Email: " . $_POST['email'] . "<br />\\n");
+   echo("Message: " . $_POST['message'] . "<br />\\n");
 ?>
