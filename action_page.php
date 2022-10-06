65639097dee30a7944debe2a876aace6bed3bfaf
BurakYILDIRAN <brkyldrn@gmail.com>
2022-10-06 13:41:37 +0300

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,5 +1,23 @@
 <?php
-   echo("Name: " . $_POST['name'] . "<br />\\n");
-   echo("Email: " . $_POST['email'] . "<br />\\n");
-   echo("Message: " . $_POST['message'] . "<br />\\n");
+
+if(empty($_POST['submit']))
+{
+	echo "Form is not submitted!";
+	exit;
+}
+if(empty($_POST["name"]) ||
+	empty($_POST["email"]))
+	{
+		echo "Please fill the form";
+		exit;
+	}
+	
+$name = $_POST["name"];
+$email = $_POST["email"];
+
+mail( 'burak.yildiran@outlook.com.tr' , 'New form submission' , 
+"New form submission: Name: $name, Email:$email"  );
+
+header('Location: thank-you.html');
+
 ?>
